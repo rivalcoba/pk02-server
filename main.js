dca2ceb1d437031f54917952ff405f6db4b92492
rivalcoba <ivan.rivalcoba@gmail.com>
2021-10-21 14:43:12 -0500

    Administrando diferentes rutas

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
 // Se importa el modulo
-import http from 'http';
-import routes from './routes.js';
 import express from 'express';
-import { nextTick } from 'process';
 
 // Creando una instancia de express
 const app = express();
-
-// Creando el primer middleware
-app.use((req, res, next) => {
-  console.log('📞 Eston en el middleware 01');
-  next();
-});
-// Otro middleware
-app.use((req, res, next) => {
-  console.log('📞 Eston en el middleware 02');
-  // Enviando una respuesta
-  res.send("<h1>Mi APP</h1>\n😁 Hola extraño");
+// Filtrando la ruta '/about'
+app.use('/about', (req, res, next) => {
+  res.send('<h1>🔦 Acerca de</h1>\n Primera aplicación en express');
 });
 
-// Se crea el server con un factory
-const server = http.createServer(app);
+// Filtrando la ruta raíz
+app.use('/', (req, res, next) => {
+  res.send('<h1>👨‍💻 Mi App </h1>\n Hola extraño 😁');
+});
 
 // Poniendo el servidor a escuchar
-// port,
-server.listen(3000, '0.0.0.0', () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('🤖 Sirviendo en el puerto: http://localhost:3000');
 });
